8651e19d2c73802cb00193985e21a655b0c19004
nagashi <ceoriley@gmail.com>
2019-03-14 05:03:22 +0000

    Week 3, Lesson 5, Had to rollback and migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,9 @@ ActiveRecord::Schema.define(version: 2019_03_14_031943) do
   enable_extension "plpgsql"
 
   create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
