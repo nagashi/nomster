ad063024953223387faec9dff5ae3320b36e7f0a
nagashi <ceoriley@gmail.com>
2019-03-20 06:10:54 +0000

    week 4, Lesson 4:14

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,23 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all
+    @places = Place.order(:name).all
   end
  
   def new
     @place = Place.new
   end
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
     
+end
+
+
+
+private
+
+def place_params
+  params.required(:place).permit(:name, :description, :address)
 end
