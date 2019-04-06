4ea5d5481e2b08431511a24e7eae6cddc9a49e56
nagashi <ceoriley@gmail.com>
2019-04-06 05:00:02 +0000

    9:29 Geocoding address

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
   has_many :comments
   has_many :photos
-  validates :name, presence: true
+      validates :name, :description, presence: true, length: { minimum: 3 }
+      validates :address, presence: true, length: { minimum: 3 }, uniqueness: { case_sensitive: false }
 end
