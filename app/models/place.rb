6e7caf84347019b3fc7a7d49ef3d768c0fc77fbd
nagashi <ceoriley@gmail.com>
2019-04-07 04:37:27 +0000

    10:33 Form submission

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
   geocoded_by :address
   after_validation :geocode
-  has_many :comments
   has_many :photos
       validates :name, :description, presence: true, length: { minimum: 3 }
       validates :address, presence: true, length: { minimum: 3 }, uniqueness: { case_sensitive: false }
