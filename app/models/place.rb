3fe33f0ebbdf20459817907c1af755dab130a33a
nagashi <ceoriley@gmail.com>
2019-04-10 04:54:14 +0000

    11:40 Emailing Basics

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,11 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :photos
+
   geocoded_by :address
   after_validation :geocode
-  has_many :photos
-      validates :name, :description, presence: true, length: { minimum: 3 }
-      validates :address, presence: true, length: { minimum: 3 }, uniqueness: { case_sensitive: false }
+
+  validates :name, :description, presence: true, length: { minimum: 3 }
+  validates :address, presence: true, length: { minimum: 3 }, uniqueness: { case_sensitive: false }
 end
